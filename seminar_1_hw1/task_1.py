79646d1ee5bf66c749fa45d2b7d8b54fc2673174
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-06 16:59:53 +0300

    Added some implementation of the task solution

--- seminar_1_hw1/task_1.py
+++ seminar_1_hw1/task_1.py
@@ -5,3 +5,30 @@
 # 7 -> да
 # 1 -> нет
 
+
+def is_holyday(day):
+    if 1 <= day <= 5:
+        return False
+    elif 6 <= day <= 7:
+        return True
+    
+
+def is_holyday2(day):
+    week_days = [1, 2, 3, 4, 5, 6, 7]
+    if day in week_days[0:4]:
+        return False
+    elif day in week_days[5:]:
+        return True
+    else:
+        return
+
+
+number_day_of_week = int(input('Введите цифру (от 1 до 7), обозначающую день недели: '))
+
+
+if is_holyday(number_day_of_week):
+    print(f'{number_day_of_week} -> да')
+else:
+    print(f'{number_day_of_week} -> нет')
+
+
