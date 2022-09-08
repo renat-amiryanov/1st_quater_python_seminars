bcbb10d152af56b38d5ebfda21e7d4071cbb251c
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-08 14:49:48 +0300

    Added some changes

--- seminar_1_hw1/task_2.py
+++ seminar_1_hw1/task_2.py
@@ -15,21 +15,22 @@ x   y   z       ¬(X ⋁ Y ⋁ Z)    ¬X ⋀ ¬Y ⋀ ¬Z
 1   1   1       0                0
 
 """
+def display_table_of_thruth():
+    print(f'x\ty\tz', end='\t\t')
+    print('¬(X ⋁ Y ⋁ Z)', end='\t')
+    print('¬X ⋀ ¬Y ⋀ ¬Z', end='\t')
+    print('¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z')
 
-print(f'x\ty\tz', end='\t\t')
-print('¬(X ⋁ Y ⋁ Z)', end='\t')
-print('¬X ⋀ ¬Y ⋀ ¬Z', end='\t')
-print('¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z')
+    print('-' * 77)
+    for x in [0, 1]:
+        for y in [0, 1]:
+            for z in [0, 1]:
+                print(f'{x}\t{y}\t{z}', end='\t\t')
+                expr1 = 1 if not (x and y and z) else 0
+                print(expr1, end='\t\t\t\t')
+                expr2 = 1 if (not x) or (not y) or (not z) else 0
+                print(expr2, end='\t\t\t\t')
+                print(1 if expr1 == expr2 else 0)
 
-print('-' * 77)
-for x in [0, 1]:
-    for y in [0, 1]:
-        for z in [0, 1]:
-            print(f'{x}\t{y}\t{z}', end='\t\t')
-            expr1 = 1 if not (x and y and z) else 0
-            print(expr1, end='\t\t\t\t')
-            expr2 = 1 if (not x) or (not y) or (not z) else 0
-            print(expr2, end='\t\t\t\t')
-            print(1 if expr1 == expr2 else 0)
-
+display_table_of_thruth()
 
