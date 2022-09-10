a90f8fb67e0c60260fc3df5aff96ee1e324d24e2
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-10 10:32:49 +0300

    Commit

--- seminar_1_hw1/task_5.py
+++ seminar_1_hw1/task_5.py
@@ -2,10 +2,19 @@
 # и находит расстояние между ними 2D пространстве.
 # Пример
 # A (3, 6); B(2, 1) -> 5.09
-# A (7, -5); B(1, -1) -> 5.09
+# A (7, -5); B(1, -1) -> 7.21
 
 import math
 
 
 def calculate_distance(a, b):
-    return math.dist(a, b)
+    # return math.dist(a, b)
+    return math.sqrt(sum((a - b) ** 2.0 for a, b in zip(a, b)))
+
+
+first_point = [float(input("Введите координату x первой точки")), float(input("Введите координату y первой точки"))]
+second_point = [float(input("Введите координату x второй точки")), float(input("Введите координату y второй точки"))]
+print(f'A {first_point}, B {second_point} -> {calculate_distance(first_point, second_point)}')
+
+for x in zip([first_point, second_point]):
+    print(type(x))
