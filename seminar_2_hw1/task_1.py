fb4f870410f977cc28d0a0b690cdf8b20d5ba4ee
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-12 14:04:37 +0300

    Commit 1

--- seminar_2_hw1/task_1.py
+++ seminar_2_hw1/task_1.py
@@ -6,3 +6,30 @@
 67.82 - 23
 0.56 -11
 """
+
+
+# Для проверки правильности пользовательского ввода
+def check_user_input(message='Enter a number, please: '):
+    is_correct = False
+    while not is_correct:
+        try:
+            user_input = input(message)
+            user_input = float(user_input)
+            is_correct = True
+        except:
+            print('Incorrect input!')
+    return user_input
+
+
+def calculate_sum(n):
+    n = abs(n)
+    value = 0
+    while n > 0:
+        value += n % 10
+        n //= 10
+    return value
+
+
+number = check_user_input()
+print(calculate_sum(number))
+
