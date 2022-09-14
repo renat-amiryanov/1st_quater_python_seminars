108740d20fec34f05be88051191c925fa9e73a89
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-14 18:15:44 +0300

    Commit

--- seminar_2_hw1/task_4.py
+++ seminar_2_hw1/task_4.py
@@ -6,3 +6,32 @@
 
 """
 
+import time
+
+
+def random(start, end):
+    """
+    Custom generator of random integer number
+    :param start:
+    :param end:
+    :return: random number between start and end
+    """
+    rand_number = time.time_ns()
+
+    # выполняем сдвиг влево на 21 бит
+    rand_number ^= (rand_number << 27)
+    rand_number ^= (rand_number >> 9)
+    rand_number ^= (rand_number << 86)
+
+    rand_number %= end
+
+    if rand_number < start:
+        rand_number += start
+    return rand_number
+
+
+for i in range(1, 10):
+    time.sleep(0.03)
+    print(random(70, 100), end=', ')
+
+
