2302178e50553c2ec49fe347cff08fdb11c6b23e
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-15 09:32:04 +0300

    Finish commit

--- seminar_2_hw1/task_4.py
+++ seminar_2_hw1/task_4.py
@@ -16,24 +16,25 @@ def random(start, end):
     :param end:
     :return: random number between start and end
     """
-    rand_number = time.time_ns()
+    is_in_range = False
 
-    # выполняем сдвиг влево на 21 бит
-    rand_number ^= (rand_number << 27)
-    rand_number ^= (rand_number >> 9)
-    rand_number ^= (rand_number << 86)
+    while not is_in_range:
+        rand_number = time.time_ns()
+        # выполняем сдвиг влево на 21 бит
+        rand_number ^= (rand_number << 21)
+        rand_number ^= (rand_number >> 35)
+        rand_number ^= (rand_number << 4)
 
-    rand_number %= end
+        rand_number %= end
 
-    if rand_number < start:
-        rand_number += start
-    if rand_number > end:
-        rand_number =
+        if rand_number < start:
+            rand_number += start
+
+        if start <= rand_number < end:
+            is_in_range = True
     return rand_number
 
 
 for i in range(1, 10):
-    time.sleep(0.003)
-    print(random(70, 100), end=', ')
-
-
+    time.sleep(0.005)
+    print(f'{random(50, 100)}', end=' ')
