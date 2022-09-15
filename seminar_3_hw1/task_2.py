0d63c2270f36da0ca1402ca1e9e314ccda7325c8
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-15 20:37:01 +0300

    Commit

--- seminar_3_hw1/task_2.py
+++ seminar_3_hw1/task_2.py
@@ -1,19 +1,39 @@
 """
 Задача 2.
-Напишите программу которая найдет произведение парт чисел списка.
-Паро считаем первый и последний элемент, второй и предпоследний и т.д.
+Напишите программу которая найдет произведение пар чисел списка.
+Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
 Пример:
 [2,3,4,5,6] => [12,15,16]
 [2,3,5,6] => [12,15]
 
 """
-numbers_list = [2, 3,4, 5, 6]
+numbers = [2, 3, 4, 5, 6]
+nums = [2, 3, 5, 6]
 
-left = 0
-right = len(numbers_list)-1
 
-for i in range(left,right):
-    print(left, right, numbers_list[left] * numbers_list[right])
-    left +=1
-    right -=1
+def multiply_pairs(values: list) -> list:
+    """
+     Функция которая находит произведение пар чисел списка
+    :param values:
+    :return: Возврашается список произведений пар
+    """
+    left = 0
+    right = len(values) - 1
+    ret_values = []
+    while left <= right:
+        ret_values.append(values[left] * values[right])
+        left += 1
+        right -= 1
+    return ret_values
+
+
+def display(vals):
+    res = multiply_pairs(vals)
+    print(f'{vals} => {res}')
+
+
+display(numbers)
+display(nums)
+
+display([1, 2, 4, 8, 16, 32, 16, 8, 4, 2, 1])
