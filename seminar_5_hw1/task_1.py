fcc7370b2807425bf4799aed5555de8933b24c51
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-23 17:01:11 +0300

    Commit

--- seminar_5_hw1/task_1.py
+++ seminar_5_hw1/task_1.py
@@ -7,10 +7,42 @@
 
 
 def demo():
+    """
+    Демонстрационная функция
+    :return:
+    """
     text = 'абвгдейка - это передача'
     sub_str = 'абв'
-    li_c = [t for t in text.split() if sub_str not in t]
-    print(f'{text} =>', *li_c)
+    value = [t for t in text.split() if sub_str not in t]
+    print(f'"{text}" => "{" ".join(value)}"')
+
+
+def remove_strings_contains_sub_string(string, sub_string):
+    """
+    Функция которая удаляет слова из текста содержащие заданную подстроку
+
+    :param string:
+    :param sub_string:
+    :return:
+    """
+    value = [s for s in string.split() if sub_string not in s]
+    return ' '.join(value)
 
 
 demo()
+
+
+def do_it():
+    print('Программа удаления из текста все слова содержашие подстроку.')
+    message = input("Введите текст: ")
+    sub = input("Введите подстроку: ")
+    if not message and not sub:
+        print("Ничего не введено. Программа завершает свою работу.")
+        return
+    output = remove_strings_contains_sub_string(message, sub)
+    print("Результат: ", end='')
+    print(''.join([output if len(output) > 0 else 'Все слова удалены.']))
+    print("Конец программы.")
+
+
+do_it()
