247a69ad56d8f5fc4c546e0f02d9b982d365e84c
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-29 17:08:11 +0300

    Commit

--- seminar_6_hw1/task_5.py
+++ seminar_6_hw1/task_5.py
@@ -1,4 +1,23 @@
 """
-5- Найти произведение пар чисел в списке.
+Задача 5.
+Найти произведение пар чисел в списке.
 Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 """
+
+def multiply_pairs():
+    n = int(input('Введите размер списка: '))
+    lst_numbers = list(map(int, input(f'Введите последовальность из {n} цифр через пробел: ').split()))[:n]
+    print(f'Список чисел: {lst_numbers}')
+
+    def gen(lst):
+        left, right = 0, len(lst) - 1
+        while left <= right:
+            yield lst[left] * lst[right]
+            left += 1
+            right -= 1
+
+    multi_lst = [multi for multi in gen(lst_numbers)]
+    print(f'Список произведений пар чисел: {multi_lst}')
+
+
+multiply_pairs()
