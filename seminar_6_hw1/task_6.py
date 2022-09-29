b285ce021629e87b3a2b4fc8d77e6512f174a283
renat-amiryanov <rfamiryanov@gmail.com>
2022-09-29 17:22:49 +0300

    Commit

--- seminar_6_hw1/task_6.py
+++ seminar_6_hw1/task_6.py
@@ -4,7 +4,17 @@
 Для N = 5: 1, -3, 9, -27, 81 и т.д.
 """
 
-n = 5
-sequence = list(map(lambda i: (-3) ** i, [i for i in range(n)]))
-sequence = list(map(str,sequence))
-print(', '.join(sequence))
+
+def create_sequence():
+    """
+    Функция которая формирует список N членов последовательности
+
+    """
+    N = int(input('Введите N: '))
+    sequence = list(map(lambda i: (-3) ** i, [i for i in range(N)]))
+    sequence = list(map(str, sequence))
+    print(f'Для {N = }: ', end='')
+    print(', '.join(sequence))
+
+
+create_sequence()
