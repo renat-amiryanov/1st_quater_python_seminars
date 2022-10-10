8aa61318d219daac664c96b077dd28001e54e7e5
renat-amiryanov <rfamiryanov@gmail.com>
2022-10-10 22:02:43 +0300

    commit

--- seminar_6_hw1/task_0.py
+++ seminar_6_hw1/task_0.py
@@ -13,71 +13,32 @@
 (2+((5-3)*(16-14)))/3 => 2
 (256 - 194 => некорректная запись скобок
 """
-import math
-
-
 def checker(expr):
+    """
+    Функция проверки корректности пар скобок () в выражении
+    :param expr:
+    :return:
+    """
     stack = []
 
     for i in range(len(expr)):
         if expr[i] == '(':
             stack.append(expr[i])
         elif expr[i] == ')':
-            stack.pop()
+            if len(stack) != 0 and stack[-1] != '(':
+                stack.pop()
+            else:
+                return False
     if stack:
         return False
     return True
 
 
-# def is_correct_input(expr):
-
-
-def solve(expr):
-    expr = expr.replace(' ', '')
-    expr = list(expr[::-1])
-
-    def get_value():
-        sign = 1
-        if expr and expr[-1] == '-':
-            expr.pop()
-            sign = -1
-        value = 0
-        while expr and expr[-1].isdigit():
-            value *= 10
-            value += int(expr.pop())
-        return sign * value
-
-    def get_term():
-        term = get_value()
-        while expr and expr[-1] in '*/':
-            op = expr.pop()
-            value = get_value()
-            if op == '*':
-                term *= value
-            else:
-                term = term / value
-        return term
-
-    ans = get_term()
-    while expr:
-        op, term = expr.pop(), get_term()
-        if op == '+':
-            ans += term
-        else:
-            ans -= term
-    return ans
-
-
-test1 = '2+2'  # 2+2 => 4;
-test2 = '1+2*3'  # 1+2*3 => 7;
-test3 = '10/2*5'  # 10/2*5 => 25;
-test4 = '10 * 5 '  # 10 * 5 * => недостаточно числовых данных
-test5 = '-5 + 5'  # -5 + 5 => 0
-test6 = 'два + три'  # два + три => неправильный ввод: нужны числа
-test7 = '(2+((5-3)*(16-14)))/3'  # (2+((5-3)*(16-14)))/3 => 2
-test8 = '(256 - 194'  # (256 - 194 => некорректная запись скобок
-
-
+def is_correct_input(expr):
+     for i in range(len(expr)):
+         if expr[i].isalpha():
+             return False
+         return True
 def get_op_priority(operation):
     """
     Функция которая возвращает приоритет операции
@@ -91,7 +52,7 @@ def get_op_priority(operation):
 
 def compute(a, b, operation):
     """
-    Функкция которая выполняет метематический операции
+    Функция которая выполняет метематический операции
     """
     if operation == '^': return a ^ b
     if operation == '*': return a * b
@@ -101,15 +62,17 @@ def compute(a, b, operation):
 
 
 def solver(expression):
-    # список для хранения номеров из выражения
-    values = []
+    # стэк для хранения чисел из выражения
+    stc_numbers = []
 
-    # списко для хранениея операций
-    operators = []
+    # стэк для хранения операторов и скобок (,+,-,*,/,^,)
+    stc_symbols = []
     i = 0
 
     if not checker(expression): return "некорректная запись скобок"
+
     if expression[-1] in '+-*/%^': return "недостаточно числовых данных"
+    if not is_correct_input(expression): return "неправильный ввод: нужны числа"
     
     if expression[0] == '-':
         val = 0
@@ -117,8 +80,7 @@ def solver(expression):
         while i <= len(expression) and expression[i].isdigit():
             val = (val * 10) + int(expression[i])
             i += 1
-
-        values.append(-1 * val)
+        stc_numbers.append(-1 * val)
 
     while i < len(expression):
 
@@ -127,41 +89,41 @@ def solver(expression):
             continue
 
         elif expression[i] == '(':
-            operators.append(expression[i])
+            stc_symbols.append(expression[i])
 
         elif expression[i].isdigit():
             val = 0
             while i < len(expression) and expression[i].isdigit():
                 val = (val * 10) + int(expression[i])
                 i += 1
-            values.append(val)
+            stc_numbers.append(val)
             i -= 1
         elif expression[i] == ')':
-            while len(operators) != 0 and operators[-1] != '(':
-                val2 = values.pop()
-                val1 = values.pop()
-                operator = operators.pop()
+            while len(stc_symbols) != 0 and stc_symbols[-1] != '(':
+                val2 = stc_numbers.pop()
+                val1 = stc_numbers.pop()
+                operator = stc_symbols.pop()
 
-                values.append(compute(val1, val2, operator))
-            operators.pop()
+                stc_numbers.append(compute(val1, val2, operator))
+            stc_symbols.pop()
 
         else:
-            while (len(operators) != 0
-                   and get_op_priority(operators[-1]) >= get_op_priority(expression[i])):
-                val2 = values.pop()
-                val1 = values.pop()
-                operator = operators.pop()
+            while (len(stc_symbols) != 0
+                   and get_op_priority(stc_symbols[-1]) >= get_op_priority(expression[i])):
+                val2 = stc_numbers.pop()
+                val1 = stc_numbers.pop()
+                operator = stc_symbols.pop()
 
-                values.append(compute(val1, val2, operator))
-            operators.append(expression[i])
+                stc_numbers.append(compute(val1, val2, operator))
+            stc_symbols.append(expression[i])
         i += 1
-    while len(operators) != 0:
-        val2 = values.pop()
-        val1 = values.pop()
-        operator = operators.pop()
+    while len(stc_symbols) != 0:
+        val2 = stc_numbers.pop()
+        val1 = stc_numbers.pop()
+        operator = stc_symbols.pop()
 
-        values.append(compute(val1, val2, operator))
-    return values[-1]
+        stc_numbers.append(compute(val1, val2, operator))
+    return stc_numbers[-1]
 
 
 test1 = '2+2'  # 2+2 => 4;
@@ -172,20 +134,28 @@ test5 = '-5 + 5'  # -5 + 5 => 0
 test6 = 'два + три'  # два + три => неправильный ввод: нужны числа
 test7 = '(2+((5-3)*(16-14)))/3'  # (2+((5-3)*(16-14)))/3 => 2
 test8 = '(256 - 194'  # (256 - 194 => некорректная запись скобок
-#
+
 res = solver(test1)
 print(f'{test1} => {res}')
+
 res = solver(test2)
 print(f'{test2} => {res}')
+
 res = solver(test3)
 print(f'{test3} => {res}')
-res = solver(test5)
-print(f'{test5} => {res}')
-res = solver(test7)
-print(f"{test7} => {res}")
+
 res = solver(test4)
 print(f"{test4} => {res}")
+
+res = solver(test5)
+print(f'{test5} => {res}')
+
+res = solver(test7)
+print(f"{test7} => {res}")
+
 res = solver(test8)
 print(f"{test8} => {res}")
 
-
+user_input = input("Введите арифметического выражени: ")
+result = solver(user_input)
+print(f'{user_input} => {result}')
