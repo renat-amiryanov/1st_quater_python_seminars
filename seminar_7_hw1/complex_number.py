f00d40dde7339fb07104db27413a7d07df8d5a20
renat-amiryanov <rfamiryanov@gmail.com>
2022-10-13 18:47:59 +0300

    commit

--- seminar_7_hw1/complex_number.py
+++ seminar_7_hw1/complex_number.py
@@ -1,5 +1,6 @@
 import re
 
+
 def mult(a, b):
     """
     Функция умножения комплексных чисел
@@ -11,7 +12,6 @@ def mult(a, b):
     print(x, x[0] * y[0] - x[1] * y[1])
     print(y, x[0] * y[1] + x[1] * y[0])
     return str(x[0] * y[0] - x[1] * y[1]) + str(x[0] * y[1] + x[1] * y[0]) + 'j'
-
 
 
 def add(a, b):
@@ -28,6 +28,7 @@ def sub(a, b):
     """
     Функция вычитания комплексных чисел
     """
+    #(a+bi)-(c+di) = (a-c)+(b-c)i
     x = [int(x) for x in re.findall('[-+]?\d+', a)]
     y = [int(y) for y in re.findall('[-+]?\d+', b)]
     return str(x[0] - y[0]) + '-' + str(x[1] - y[1]) + 'j'
@@ -37,4 +38,5 @@ def div(a, b):
     """
     Функция деления комплексных чисел
     """
+    # (a+bi)/(c+di) = (ac+bd)/(c^2+d^2)+(bc-ab)/(c^2+d^2)i
     pass
--- seminar_7_hw1/task_1.py
+++ seminar_7_hw1/task_1.py
@@ -9,13 +9,29 @@ import re
 
 import complex_number
 
-a = '-3-2j'
-b = '3+2j'
-mult = complex_number.mult(a, b)
+a = '10_руб.'
+b = '5+7j'
+# mult = complex_number.mult(a, b)
 # add = complex_number.add(a, b)
 # sub = complex_number.sub(a, b)
 
-print(mult, complex(a)*complex(b))
+# print(mult, complex(a)*complex(b))
 # print(add, complex(a)+complex(b))
 # print(sub,complex(a)-complex(b))
 
+
+text = '-10+10'
+
+container = []
+val = 0
+sing = 1
+print(f'{container=}')
+for t in text:
+
+    if t.isdigit():
+        val = sing * val * 10 + int(t)
+    else:
+        print(val)
+        continue
+        container.append(val)
+print(f'{container=}')
